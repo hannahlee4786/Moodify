64e9555dd6148e31329e5b223430df6f0c6131a2
Hannah Lee <hlee4786@usc.edu>
2025-05-23 00:08:08 -0700

    moved post files

--- Moodify/Later/PostCell.swift
+++ Moodify/Later/PostCell.swift
@@ -1,27 +1,27 @@
+////
+////  PostCell.swift
+////  Moodify
+////
+////  Created by Hannah Lee on 5/17/25.
+////
 //
-//  PostCell.swift
-//  Moodify
+//import SwiftUI
 //
-//  Created by Hannah Lee on 5/17/25.
-//
-
-import SwiftUI
-
-struct PostCell: View {
-    let post: Post
-    
-    var body: some View {
-        NavigationLink(value: post) {
-            AsyncImage(url: URL(string: post.imageURL)) { post in
-                post
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            } placeholder: {
-//                Color.gray.opacity(0.3) // a simple placeholder
-            }
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .frame(height: 160)
-            .clipped()
-        }
-    }
-}
+//struct PostCell: View {
+//    let post: Post
+//    
+//    var body: some View {
+//        NavigationLink(value: post) {
+//            AsyncImage(url: URL(string: post.imageURL)) { post in
+//                post
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fill)
+//            } placeholder: {
+////                Color.gray.opacity(0.3) // a simple placeholder
+//            }
+//            .frame(minWidth: 0, maxWidth: .infinity)
+//            .frame(height: 160)
+//            .clipped()
+//        }
+//    }
+//}
--- Moodify/Later/PostGridView.swift
+++ Moodify/Later/PostGridView.swift
@@ -1,34 +1,34 @@
 //
-//  PostGridView.swift
-//  Moodify
+////  PostGridView.swift
+////  Moodify
+////
+////  Created by Hannah Lee on 5/7/25.
+////
 //
-//  Created by Hannah Lee on 5/7/25.
+//import SwiftUI
 //
-
-import SwiftUI
-
-struct PostGridView: View {
-    @ObservedObject var viewModel: UserProfileViewModel
-    @State private var viewDidLoad = false
-    var userId: String
-    
-    let items: [GridItem] = [
-        GridItem(.flexible(minimum: 120)),
-        GridItem(.flexible(minimum: 120))
-    ]
-    
-    var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            if viewModel.isLoading {
-                ProgressView()
-            } else {
-                LazyVGrid(columns: items, spacing: 10) {
-                    ForEach(viewModel.user?.posts ?? [], id: \.id) { post in
-                        PostCell(post: post)
-                    }
-                }
-                .padding()
-            }
-        }
-    }
-}
+//struct PostGridView: View {
+//    @ObservedObject var viewModel: UserProfileViewModel
+//    @State private var viewDidLoad = false
+//    var userId: String
+//    
+//    let items: [GridItem] = [
+//        GridItem(.flexible(minimum: 120)),
+//        GridItem(.flexible(minimum: 120))
+//    ]
+//    
+//    var body: some View {
+//        ScrollView(.vertical, showsIndicators: false) {
+//            if viewModel.isLoading {
+//                ProgressView()
+//            } else {
+//                LazyVGrid(columns: items, spacing: 10) {
+//                    ForEach(viewModel.user?.posts ?? [], id: \.id) { post in
+//                        PostCell(post: post)
+//                    }
+//                }
+//                .padding()
+//            }
+//        }
+//    }
+//}
