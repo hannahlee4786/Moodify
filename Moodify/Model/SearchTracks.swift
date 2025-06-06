77f9839e2b35b21d3bf29d3dae21cc1b29f313e3
Hannah Lee <hlee4786@usc.edu>
2025-06-06 11:29:15 -0700

    organized files/folders

---
+++ Moodify/Model/SearchTracks.swift
@@ -0,0 +1,19 @@
+//
+//  SearchTracks.swift
+//  Moodify
+//
+//  Created by Hannah Lee on 5/31/25.
+//
+
+import Foundation
+
+struct TrackObject: Codable {
+    let album: Album
+    let artists: [SimplifiedArtistObject]
+    let id: String
+    let name: String
+}
+
+struct SearchTracks: Codable {
+    let items: [TrackObject]
+}
